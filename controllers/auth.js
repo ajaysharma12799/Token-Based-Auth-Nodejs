4b928c576ccc38e3192d3dac2cd915aa7bf685a8
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-07-10 18:00:34 +0530

    password hashing is done, completed signup part

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const expressJWT = require('express-jwt');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
     const errors = validationResult(req);
     if( !errors.isEmpty() ) {
         return res.status(422).json({
@@ -12,7 +13,14 @@ exports.signup = (req, res) => {
         })
     }
 
-    const user = new User(req.body);
+    const salt = await bcrypt.genSalt(10); // GENERATING SALT
+    const hashedPassword = await bcrypt.hash(req.body.password, salt); // HASHING PASSWORD
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
     user.save( (error, user) => {
         if(error) {
             return res.status(400).json('NOT ABLE TO SAVE USER IN DATABASE');
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,9 @@ const User = require('../models/user');
 const { signup, signin } = require('../controllers/auth');
 
 router.post('/signup',[
-    check('name', 'name should be atleast 5 character').isEmpty().isLength({min: 5, max: 30}),
-    check('email', 'email should be correct').isEmpty().isEmail(),
-    check('password', 'password be above 7 character').isEmpty().isLength({min: 7, max: 1024})
+    check('name', 'name should be atleast 5 character').isLength({min: 5, max: 30}),
+    check('email', 'email should be correct').isEmail(),
+    check('password', 'password be above 7 character').isLength({min: 7, max: 1024})
 ] , signup);
 
 router.post('/signin', signin);
